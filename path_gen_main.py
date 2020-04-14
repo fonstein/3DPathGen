63b5d2bd670a9593d621552a95d22b048aaf7c35
fonstein <ingrfjon@gmail.com>
2020-04-14 09:30:49 +0200

    Add inital script for interaction with FreeCAD

---
+++ path_gen_main.py
@@ -0,0 +1,23 @@
+import sys
+import FreeCAD
+import socket                       # Makes it possible to import script as module
+
+# Has to be added to the path for FreeCAD to find the scripts
+sys.path.append("C:/Users/ingridon/OneDrive/Documents/PhD/Code and shits/PathGeneration")
+
+__title__ = "PathGeneration"
+__author__ = "Ingrid Fjordheim Onstein"
+__version__ = "0.1"
+__doc__ = """Tool for generating paths for deburring based on edges in STEP CAD models"""
+
+def main():
+    FreeCAD.Console.PrintMessage("\n\n============== START ==============")
+    selected = FreeCADGui.Selection.getSelectionEx()
+    edge = selected[0].SubObjects
+
+    FreeCAD.Console.PrintMessage("\n Selected object: " + str(edge))
+
+    FreeCAD.Console.PrintMessage("\n\n============= FINISHED ============")
+
+if __name__ == "__main__":
+    main()
